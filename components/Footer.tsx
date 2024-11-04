a882338c79f6e351654d7363e45bd7be02f70bf2
Piyush <piyushbhagchandani.64@gmail.com>
2024-11-04 13:24:31 +0530

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,9 @@
 export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-xs">
+      {/* <small className="mb-2 block text-xs">
         &copy; 2024 Image Rater. All rights reserved.
-      </small>
+      </small> */}
       <p className="text-xs">
         <span className="font-semibold">built by</span> <a className="underline cursor-pointer" href="">Piyush Bhagchandani</a>
       </p>
